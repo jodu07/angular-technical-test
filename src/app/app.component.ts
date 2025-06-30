324cc701ca10a655b14e9f8075606934131a8a1f
jodu07 <jhonnat.dussan@gmail.com>
2025-06-30 18:03:58 +0200

    angular material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FilterPanelComponent } from "./features/hotels/components/filter-panel/filter-panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FilterPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/features/hotels/components/filter-panel/filter-panel.component.ts
+++ src/app/features/hotels/components/filter-panel/filter-panel.component.ts
@@ -1,11 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatFormField,
+  MatInputModule,
+  MatLabel,
+} from '@angular/material/input';
 
 @Component({
   selector: 'app-filter-panel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInputModule,
+  ],
   templateUrl: './filter-panel.component.html',
   styleUrl: './filter-panel.component.scss',
 })
-export class FilterPanelComponent {}
+export class FilterPanelComponent {
+  @Output() filtersChanged = new EventEmitter<any>();
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [''],
+      stars: this.fb.group({
+        1: [false],
+        2: [false],
+        3: [false],
+        4: [false],
+        5: [false],
+      }),
+      minRating: [0],
+      maxPrice: [1000],
+    });
+
+    this.form.valueChanges.subscribe((value) => {
+      const selectedStars = Object.entries(value.stars)
+        .filter(([_, v]) => v)
+        .map(([k, _]) => +k);
+
+      this.filtersChanged.emit({
+        name: value.name.toLowerCase(),
+        stars: selectedStars,
+        minRating: value.minRating,
+        maxPrice: value.maxPrice,
+      });
+    });
+  }
+}
