73da5ff4c6918f7878113618af94184b0c7e64eb
jodu07 <jhonnat.dussan@gmail.com>
2025-06-30 23:50:43 +0200

    getHotels method added in Hotel.service

--- src/app/features/hotels/services/hotel.service.ts
+++ src/app/features/hotels/services/hotel.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Hotel } from '../models/hotel.model';
 import { computed, effect, signal as createSignal } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,9 @@ export class HotelService {
     this.http
       .get<Hotel[]>(this.baseUrl)
       .subscribe((data) => this._hotels.set(data));
+  }
+
+  getHotels(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(this.baseUrl);
   }
 }
