ef80af335aed38cfa3cee89277b47eb4034b1fbd
jodu07 <jhonnat.dussan@gmail.com>
2025-07-01 09:59:37 +0200

    Unit tests added in hotel-card, hotel-list-panel and hotel-service components

--- src/app/features/hotels/components/filter-panel/filter-panel.component.spec.ts
+++ src/app/features/hotels/components/filter-panel/filter-panel.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilterPanelComponent } from './filter-panel.component';
 
-fdescribe('FilterPanelComponent', () => {
+describe('FilterPanelComponent', () => {
   let component: FilterPanelComponent;
   let fixture: ComponentFixture<FilterPanelComponent>;
 
--- src/app/features/hotels/components/hotel-card/hotel-card.component.spec.ts
+++ src/app/features/hotels/components/hotel-card/hotel-card.component.spec.ts
@@ -1,10 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HotelCardComponent } from './hotel-card.component';
+import { Hotel } from '../../models/hotel.model';
 
 describe('HotelCardComponent', () => {
   let component: HotelCardComponent;
   let fixture: ComponentFixture<HotelCardComponent>;
+
+  const mockHotel: Hotel = {
+    id: '1',
+    name: 'Hotel Test',
+    image: 'https://example.com/image.jpg',
+    address: '123 Test Street',
+    stars: 4,
+    rate: 4.2,
+    price: 200,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +24,7 @@ describe('HotelCardComponent', () => {
 
     fixture = TestBed.createComponent(HotelCardComponent);
     component = fixture.componentInstance;
+    component.hotel = mockHotel;
     fixture.detectChanges();
   });
 
